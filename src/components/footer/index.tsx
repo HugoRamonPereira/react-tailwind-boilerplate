bd8d92db1ff4c6378d774a486ace178f302dce77
Hugo Ramon Pereira <hramonp-dev@outlook.com>
2023-12-07 16:12:35 -0300

    footer created

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,3 +1,17 @@
 export function Footer() {
-  return <h1>footer</h1>;
+  const year = new Date().getFullYear();
+
+  return (
+    <footer className='dark:bg-gray-900'>
+      <div className='w-full max-w-screen-xl mx-auto p-3 md:py-5'>
+        <span className='block text-sm text-gray-500 sm:text-center dark:text-gray-300 font-extralight tracking-wide'>
+          © {year}{' '}
+          <a href='https://flowbite.com/' className='hover:underline'>
+            Hugo Ramon Pereira
+          </a>
+          . All Rights Reserved.
+        </span>
+      </div>
+    </footer>
+  );
 }
